c6084c373800a34c8dda076aac1dabbf70626260
moe <you@example.com>
2023-04-10 18:16:40 +0700

    Updates

--- src/assets/script/shuffle-array.js
+++
@@ -1,9 +0,0 @@
-function shuffleArray(array) {
-  let arr2 = [...array];
-    for (let i = arr2.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [arr2[i], arr2[j]] = [arr2[j], arr2[i]];
-    }
-    return arr2;
-}
-export default shuffleArray;
